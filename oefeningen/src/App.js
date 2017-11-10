5d5c78deaeeb5623f5bbe13e2d94b937e3f56e79
manarkbyeh <manar.kbyeh@student.kdg.be>
2017-11-10 19:20:46 +0100

    add and delete

--- oefeningen/src/App.js
+++ oefeningen/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
-
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component {
@@ -20,17 +20,17 @@ class App extends Component {
   	this.setState({
   			 projects: [
       {
-
+        id:uuid.v4(),
         title: 'Business Website',
         category: 'Web Deisgn'
       },
       {
-     
+        id:uuid.v4(),
         title: 'Social App',
         category: 'Mobile Development'
       },
       {
-    
+        id:uuid.v4(),
         title: 'Ecommerce Shopping Cart',
         category: 'Web Development'
       }
@@ -45,12 +45,20 @@ class App extends Component {
   this.setState({projects:projects});
 
   }
+  handleDeleteProject(id){
+  	let projects=this.state.projects;
+  	let index=projects.findIndex(x=>x.id===id);
+  	projects.splice(index,1);
+  	this.setState({projects:projects});
+
+  }
+
     render() {
     return (
       <div className="App">
         my projects
         <AddProject AddProject={this.handleAddProject.bind(this)}/>
-        <Projects projects={this.state.projects} />
+        <Projects projects={this.state.projects } onDelete={this.handleDeleteProject.bind(this)} />
     
       </div>
     );
--- oefeningen/src/Components/AddProject.js
+++ oefeningen/src/Components/AddProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
+import uuid from 'uuid';
 
 class AddProject extends Component {
   constructor(){
@@ -22,6 +23,7 @@ handleSubmit(e){
  }else{
   this.setState({
     newProject:{
+      id:uuid.v4(),
       title:this.refs.title.value,
       category:this.refs.category.value
 
